72075aa8d0dd8f9e9db4b9c8c33ee2788b7c2f6b
Hussin Sherif <hussinsherifp@gmail.com>
2023-08-23 18:01:26 +0200

    .

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -1,11 +1,8 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:naqaa/app/di/dependency_injection.dart';
 import 'package:naqaa/app/router/app_router.dart';
-import 'package:naqaa/presentation/blocs/user/user_cubit.dart';
 import 'package:naqaa/presentation/theme/app_theme.dart';
 
 import '../app/constants.dart';
@@ -31,17 +28,14 @@ class _MyAppState extends State<MyApp> {
     return ScreenUtilInit(
       designSize: const Size(360, 800),
       minTextAdapt: true,
-      builder: (context, child) => BlocProvider(
-        create: (context) => instance<UserCubit>()..onAppStart(),
-        child: MaterialApp.router(
-          localizationsDelegates: context.localizationDelegates,
-          supportedLocales: context.supportedLocales,
-          locale: context.locale,
-          title: Constants.appName,
-          debugShowCheckedModeBanner: false,
-          theme: appThemeData(),
-          routerConfig: AppRouter.appRouter,
-        ),
+      builder: (context, child) => MaterialApp.router(
+        localizationsDelegates: context.localizationDelegates,
+        supportedLocales: context.supportedLocales,
+        locale: context.locale,
+        title: Constants.appName,
+        debugShowCheckedModeBanner: false,
+        theme: appThemeData(),
+        routerConfig: AppRouter.appRouter,
       ),
     );
   }
