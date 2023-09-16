9e5c373d549101e44521dd74e4df154ae12ed064
Hussin Sherif <hussinsherifp@gmail.com>
2023-09-16 22:49:03 +0200

    Fixed Statistics datatypes

--- lib/data/models/statistic_model.dart
+++ lib/data/models/statistic_model.dart
@@ -27,26 +27,36 @@ class StatisticModel {
 
   factory StatisticModel.fromMap(Map<dynamic, dynamic> map) {
     return StatisticModel(
-      tempHighest: map[FirebaseConstants.tempHighest]?.toInt(),
-      tempLowest: map[FirebaseConstants.tempLowest]?.toInt(),
+      tempHighest: map[FirebaseConstants.tempHighest],
+      tempLowest: map[FirebaseConstants.tempLowest],
       tempAverage: map[FirebaseConstants.tempAverage] != null
-          ? double.parse(map[FirebaseConstants.tempAverage])
+          ? map[FirebaseConstants.tempAverage] is String
+              ? double.parse(map[FirebaseConstants.tempAverage])
+              : map[FirebaseConstants.tempAverage].toDouble()
           : null,
-      tdsHighest: map[FirebaseConstants.tdsHighest]?.toInt(),
-      tdsLowest: map[FirebaseConstants.tdsLowest]?.toInt(),
+      tdsHighest: map[FirebaseConstants.tdsHighest],
+      tdsLowest: map[FirebaseConstants.tdsLowest],
       tdsAverage: map[FirebaseConstants.tdsAverage] != null
-          ? double.parse(map[FirebaseConstants.tdsAverage])
+          ? map[FirebaseConstants.tdsAverage] is String
+              ? double.parse(map[FirebaseConstants.tdsAverage])
+              : map[FirebaseConstants.tdsAverage].toDouble()
           : null,
       phHighest: map[FirebaseConstants.phHighest] != null
-          ? double.parse(map[FirebaseConstants.phHighest])
+          ? map[FirebaseConstants.phHighest] is String
+              ? double.parse(map[FirebaseConstants.phHighest])
+              : map[FirebaseConstants.phHighest].toDouble()
           : null,
       phLowest: map[FirebaseConstants.phLowest] != null
-          ? double.parse(map[FirebaseConstants.phLowest])
+          ? map[FirebaseConstants.phLowest] is String
+              ? double.parse(map[FirebaseConstants.phLowest])
+              : map[FirebaseConstants.phLowest].toDouble()
           : null,
       phAverage: map[FirebaseConstants.phAverage] != null
-          ? double.parse(map[FirebaseConstants.phAverage])
+          ? map[FirebaseConstants.phAverage] is String
+              ? double.parse(map[FirebaseConstants.phAverage])
+              : map[FirebaseConstants.phAverage].toDouble()
           : null,
-      waterQuality: map[FirebaseConstants.waterQuality]?.toInt(),
+      waterQuality: map[FirebaseConstants.waterQuality],
     );
   }
 }
